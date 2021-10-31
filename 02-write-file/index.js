0b59f4ba79ef771fed96b05d9ebb4be0bcdb7af8
Rustam Satlykov <satlikov.rustam.mail@gmail.com>
2021-10-31 15:13:57 +0500

    feat: add 02-write-file full-solution (Вс, окт 31, 2021  3:13:57)

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const fs = require('fs');
 const filePath = path.join(__dirname, 'text.txt');
 const writeStream = fs.createWriteStream(filePath);
-/* const readStream = fs.createReadStream(filePath); */
+
 
 fs.writeFile(filePath, '', (err) => {
   if (err) {
@@ -14,14 +14,23 @@ fs.writeFile(filePath, '', (err) => {
 
 const rl = readline.createInterface({ input, output });
 
-rl.question('Enter text: ', (answer) => {
-  if (answer === 'exit') {
-    console.log('Good buy!');
+function questions() {
+  rl.question('Enter text: ', (answer) => {
+    if (answer === 'exit') {
+      console.log('Good buy!');
+  
+      rl.close();
+    } else {
+      writeStream.write(`${answer}\n`);
+  
+      console.log(`Thank you for entering text: (${answer})`);
+      questions();
+    }
+  });
+}
 
-    rl.close();
-  } else {
-    writeStream.write(`${answer}\n`);
-
-    console.log(`Thank you for entering text: (${answer})`);
-  }
+rl.on('close', () => {
+  console.log('\nGood buy!');
 });
+
+questions();
