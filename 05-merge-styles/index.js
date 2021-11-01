1026969333c734be2a0918e7ced3ef6fff94f8d5
Rustam Satlykov <satlikov.rustam.mail@gmail.com>
2021-11-01 20:27:26 +0500

    feat: add solution 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.writeFile('./05-merge-styles/project-dist/bundle.css', '', (err) => {
+  err ? console.log(err) : null;
+});
+
+fs.readdir('./05-merge-styles/styles', (err, data) => {
+  err ? console.log(err) : null;
+
+  const writeStream = fs.createWriteStream(
+    './05-merge-styles/project-dist/bundle.css'
+  );
+
+  data.forEach((file) => {
+    let fileName = path.parse(file).base;
+
+    const readStream = fs.createReadStream(
+      `./05-merge-styles/styles/${fileName}`
+    );
+
+    if (path.extname(file).toLowerCase() === '.css') {
+      readStream.pipe(writeStream);
+    }
+  });
+  console.log('Bundle ready!');
+});
