8f942725217bd605100e72d44aa9fcb47211b013
Rustam Satlykov <satlikov.rustam.mail@gmail.com>
2021-11-02 22:15:47 +0500

    fix: solution 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,11 +2,7 @@ const path = require('path');
 const fs = require('fs');
 const filePath = path.join(__dirname, 'text.txt');
 
-fs.readFile(filePath, (err, content) => {
-  if (err) {
-    throw err;
-  }
-
-  const data = Buffer.from(content);
-  console.log(data.toString());
+const readStream = fs.createReadStream(filePath);
+readStream.on('data', (chunk) => {
+  console.log(chunk.toString());
 });
