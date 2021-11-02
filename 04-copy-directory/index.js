0fe486911217f9ddba478aa0028dbcb0e1262453
Rustam Satlykov <satlikov.rustam.mail@gmail.com>
2021-11-02 23:14:20 +0500

    fix: solution 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,19 +3,39 @@ const path = require('path');
 const srcFile = './04-copy-directory/files';
 const srcFileCppy = './04-copy-directory/files-copy';
 
-fs.mkdir(srcFileCppy, () => {});
+fs.stat('./04-copy-directory/files-copy', function (err) {
+  if (!err) {
+    fs.readdir('./04-copy-directory/files-copy', (err, data) => {
+      err ? console.log(err) : null;
+      data.forEach((file) => {
+        let fileName = path.parse(file).base;
+        fs.unlink(`${srcFileCppy}/${fileName}`, (err) => {
+          err ? console.log(err) : null;
+        });
+      });
+    });
 
-fs.readdir('./04-copy-directory/files', (err, data) => {
-  err ? console.log(err) : null;
-  data.forEach((file) => {
-    let fileName = path.parse(file).base;
-    fs.copyFile(
-      `${srcFile}/${fileName}`,
-      `${srcFileCppy}/${fileName}`,
-      (err) => {
-        err ? console.log(err) : null;
-      }
-    );
-  });
-  console.log('Finished copying!');
+    copyFiles();
+  } else if (err.code === 'ENOENT') {
+    fs.mkdir(srcFileCppy, () => {});
+
+    copyFiles();
+  }
 });
+
+function copyFiles() {
+  fs.readdir('./04-copy-directory/files', (err, data) => {
+    err ? console.log(err) : null;
+    data.forEach((file) => {
+      let fileName = path.parse(file).base;
+      fs.copyFile(
+        `${srcFile}/${fileName}`,
+        `${srcFileCppy}/${fileName}`,
+        (err) => {
+          err ? console.log(err) : null;
+        }
+      );
+    });
+    console.log('Finished copying!');
+  });
+}
