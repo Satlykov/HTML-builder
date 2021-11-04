8072e68b22acbbc1898de1cf75bd4352a5da1fe2
Rustam Satlykov <satlikov.rustam.mail@gmail.com>
2021-11-04 17:12:34 +0500

    fix: 05-merge-styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,8 +1,21 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.writeFile('./05-merge-styles/project-dist/bundle.css', '', (err) => {
-  err ? console.log(err) : null;
+fs.stat('./05-merge-styles/project-dist/bundle.css', function(err) {  
+  if (!err) {
+    fs.unlink('./05-merge-styles/project-dist/bundle.css', (err) => {
+      err ? console.log(err) : null;
+    });
+    fs.writeFile('./05-merge-styles/project-dist/bundle.css', '', (err) => {
+      err ? console.log(err) : null;
+    });
+    console.log('Refreshed bundle.css')
+  } else {
+    fs.writeFile('./05-merge-styles/project-dist/bundle.css', '', (err) => {
+      err ? console.log(err) : null;
+    });
+      console.log('Created new bundle.css')
+  }
 });
 
 fs.readdir('./05-merge-styles/styles', (err, data) => {
